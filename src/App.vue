e2070486824c30b5ddf11a4935eebc9539c2a42d
zhm1ng <vhming@hotmail.com>
2024-10-28 06:58:31 +0700

    =)))

--- src/App.vue
+++ src/App.vue
@@ -17,12 +17,8 @@
         </section>
       </div>
       <!-- 移动端菜单按钮 -->
-      <Icon
-        class="menu"
-        size="24"
-        v-show="!store.backgroundShow"
-        @click="store.mobileOpenState = !store.mobileOpenState"
-      >
+      <Icon class="menu" size="24" v-show="!store.backgroundShow"
+        @click="store.mobileOpenState = !store.mobileOpenState">
         <component :is="store.mobileOpenState ? CloseSmall : HamburgerButton" />
       </Icon>
       <!-- 页脚 -->
@@ -137,11 +133,13 @@ onBeforeUnmount(() => {
   transition: transform 0.3s;
   animation: fade-blur-main-in 0.65s cubic-bezier(0.25, 0.46, 0.45, 0.94) forwards;
   animation-delay: 0.5s;
+
   .container {
     width: 100%;
     height: 100vh;
     margin: 0 auto;
     padding: 0 0.5vw;
+
     .all {
       width: 100%;
       height: 100%;
@@ -151,6 +149,7 @@ onBeforeUnmount(() => {
       justify-content: center;
       align-items: center;
     }
+
     .more {
       position: fixed;
       top: 0;
@@ -162,10 +161,12 @@ onBeforeUnmount(() => {
       z-index: 2;
       animation: fade 0.5s;
     }
+
     @media (max-width: 1200px) {
       padding: 0 2vw;
     }
   }
+
   .menu {
     position: absolute;
     display: flex;
@@ -180,72 +181,96 @@ onBeforeUnmount(() => {
     border-radius: 6px;
     transition: transform 0.3s;
     animation: fade 0.5s;
+
     &:active {
       transform: scale(0.95);
     }
+
     .i-icon {
       transform: translateY(2px);
     }
+
     @media (min-width: 721px) {
       display: none;
     }
   }
+
   @media (max-height: 720px) {
     overflow-y: auto;
     overflow-x: hidden;
+
     .container {
       height: 721px;
+
       .more {
         height: 721px;
         width: calc(100% + 6px);
       }
+
       @media (min-width: 391px) {
         // w 1201px ~ max
         padding-left: 0.7vw;
         padding-right: 0.25vw;
-        @media (max-width: 1200px) { // w 1101px ~ 1280px
+
+        @media (max-width: 1200px) {
+          // w 1101px ~ 1280px
           padding-left: 2.3vw;
           padding-right: 1.75vw;
         }
-        @media (max-width: 1100px) { // w 993px ~ 1100px
+
+        @media (max-width: 1100px) {
+          // w 993px ~ 1100px
           padding-left: 2vw;
           padding-right: calc(2vw - 6px);
         }
-        @media (max-width: 992px) { // w 901px ~ 992px
+
+        @media (max-width: 992px) {
+          // w 901px ~ 992px
           padding-left: 2.3vw;
           padding-right: 1.7vw;
         }
-        @media (max-width: 900px) { // w 391px ~ 900px
+
+        @media (max-width: 900px) {
+          // w 391px ~ 900px
           padding-left: 2vw;
           padding-right: calc(2vw - 6px);
         }
       }
     }
+
     .menu {
       top: 605.64px; // 721px * 0.84
       left: 170.5px; // 391 * 0.5 - 25px
+
       @media (min-width: 391px) {
         left: calc(50% - 25px);
       }
     }
+
     .f-ter {
       top: 675px; // 721px - 46px
+
       @media (min-width: 391px) {
         padding-left: 6px;
       }
     }
   }
+
   @media (max-width: 390px) {
     overflow-x: auto;
+
     .container {
       width: 391px;
     }
+
     .menu {
       left: 167.5px; // 391px * 0.5 - 28px
     }
+
     .f-ter {
       width: 391px;
     }
+
     @media (min-height: 721px) {
       overflow-y: hidden;
     }
