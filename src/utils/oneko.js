c80742a953951cf0ecc1d8680bdda8fdb4ec4603
zhm1ng <vhming@hotmail.com>
2024-11-02 11:54:41 +0700

    Update oneko.js

--- src/utils/oneko.js
+++ src/utils/oneko.js
@@ -94,7 +94,7 @@
     nekoEl.style.top = `${nekoPosY - 16}px`;
     nekoEl.style.zIndex = 2147483647;
 
-    let nekoFile = "../../public/images/icon/oneko.gif"
+    let nekoFile = "../../images/icon/oneko.gif"
     const curScript = document.currentScript
     if (curScript && curScript.dataset.cat) {
       nekoFile = curScript.dataset.cat
